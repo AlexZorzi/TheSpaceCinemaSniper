62ffde873652c18eed018d34e818a7ca45bc0f55
Alex Zorzi <info@alexzorzi.it>
2023-04-02 17:54:11 +0200

    add Seating (2/2)
    Signed-off-by: Alex Zorzi <info@alexzorzi.it>

--- src/Seating.py
+++ src/Seating.py
@@ -1,3 +1,4 @@
+import math
 from math import floor
 
 
@@ -12,32 +13,61 @@ class Seating:
 
     def __init__(self, json):
         self.json = json
-        self.printandClean()
-    def printandClean(self):
-        for r_row, row in enumerate(self.json["seating_data"]["rows"]):
-            print(row["row_label"], end=" ")
-            row["columns"].reverse()
-            for i, column in enumerate(row["columns"]):
-                if column == None:
-                    self.json["seating_data"]["rows"][r_row]["columns"].pop(i)
+        self.y = self.json["seating_data"]['total_rows']
+        self.x = self.json["seating_data"]['total_columns']  # off by one
+        row_i = 0
+        rows = self.json["seating_data"]["rows"]
+        while row_i < len(self.json["seating_data"]["rows"]):
+            if all(v is None for v in rows[row_i]["columns"]):
+                self.json["seating_data"]["rows"].pop(row_i)
+                continue
+
+            print(rows[row_i]["row_label"], end=" ")
+            rows[row_i]["columns"].reverse()
+            seat_i = 0
+            seats = rows[row_i]["columns"]
+            while seat_i < len(rows[row_i]["columns"]):
+                if seats[seat_i] is None:
+                    self.json["seating_data"]["rows"][row_i]["columns"].pop(seat_i)
                     continue
-                if column["status"] == 2 or column["status"] == 1 or column["status"] == 7:
+                if seats[seat_i]["status"] != 0:
                     print(Seating.FAIL, end=" ")
-                elif column["area_id"] == "vip":
+                elif seats[seat_i]["area_id"] == "vip":
                     print(Seating.WARNING, end=" ")
-                elif column["area_id"] == "special":
+                elif seats[seat_i]["area_id"] == "special":
                     print(Seating.OKCYAN, end=" ")
-                elif column["area_id"] == "standard":
+                elif seats[seat_i]["area_id"] == "standard":
                     print(Seating.OKBLUE, end=" ")
-                print(column["name"], end=Seating.ENDC+" ")
+                print(seats[seat_i]["name"], end=Seating.ENDC + " ")
+                seat_i += 1
             print()
+            row_i += 1
 
     def selectBestSeatsByN(self, nposti):
-        len_rows = len(self.json["seating_data"])
-        if len_rows % 2 == 0:
-            middle = floor(len_rows / 2)
-        else:
-            middle = floor(len_rows / 2 + 0.5)
-        
+        half_y = floor(self.y / 2) + (self.y % 2)
+        half_x = floor(self.x / 2) + (self.x % 2)
+        bestSeats = {"distance": 999, "seats": []}
+        for row_index, row in enumerate(self.json["seating_data"]["rows"]):
+            for seat_index, seat in enumerate(row["columns"]):
+                # if seat_index + nposti <= len(row["columns"]) -1:
 
+                center_seat_index = ((seat_index * 2) + (nposti -1)) / 2
+                # sqrt( (x1 - x2)^2 + (y1 - y2)^2 )
+                distance = math.sqrt(((center_seat_index - half_x) ** 2) + ((row_index + 1 - half_y) ** 2))
+                if bestSeats["distance"] <= distance:
+                    continue
 
+                selected = 0
+                selected_data = []
+                while selected < nposti:
+                    if selected + seat_index <= len(row["columns"]) - 1 and row["columns"][selected + seat_index]["status"] == 0:
+                        selected_data += [row["columns"][selected + seat_index]]
+                    else:
+                        break
+                    selected += 1
+                if selected == nposti:
+                    bestSeats["seats"] = selected_data
+                    bestSeats["distance"] = distance
+
+        for seat in bestSeats["seats"]:
+            print(seat["name"])
