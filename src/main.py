3f1a2da924c940fa60c215c46cd0d228fbae8e6c
Alex Zorzi <info@alexzorzi.it>
2023-04-03 21:24:21 +0200

    main: credentials and checkout handling
    Signed-off-by: Alex Zorzi <info@alexzorzi.it>

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
+import json
 from SpaceCinema import SpaceCinema
+from selenium import webdriver
+
 
 # Demo Work in progress
 
 def main():
-    #username = input("username: ")
-    #password = input("password: ")
-    sc = SpaceCinema("", "", debug=False)
+    f = open("credentials.json")
+    cred = json.load(f)
+    sc = SpaceCinema(cred["user"], cred["password"], debug=False)
     sc.login()
     sc.loadAvailCinemas()
 
@@ -14,7 +17,7 @@ def main():
     for cinema in sc.cinemas.keys():
         print(cinema+") "+sc.cinemas[cinema])
         i +=1
-    cinema_selected = "23" #input("cinema: ")
+    cinema_selected = input("cinema: ")
     sc.loadfilmlistbyCinema(cinema_selected)
 
     print("seleziona un film:")
@@ -22,25 +25,40 @@ def main():
     for film in sc.films:
         print(str(filmn)+") "+ film.title)
         filmn += 1
-    film_selected = 0 #int(input("film: "))
+    film_selected = int(input("film: "))
 
     print("seleziona un giorno:")
     giornon = 0
-    for giorno in sc.films[film_selected].showings:
+    showings = sc.films[film_selected].showings
+    for giorno in showings:
         print(str(giornon) + ") " + giorno.date_long)
         giornon += 1
-    giorno_selected = 0 #int(input("giorno: "))
+    giorno_selected = int(input("giorno: "))
 
     print("seleziona un orario:")
     orarion = 0
-    for orario in sc.films[film_selected].showings[giorno_selected].times:
+    giorno_data = showings[giorno_selected]
+    for orario in giorno_data.times:
         print(str(orarion) + ") " + orario.time + " sala "+orario.screen_number)
         orarion += 1
-    orario_selected = 0 #int(input("orario: "))
+    orario_selected = int(input("orario: "))
+    orario_data = giorno_data.times[orario_selected]
 
-    #print(sc.BASE + sc.films[film_selected].showings[giorno_selected].times[orario_selected].getLink())
-    seating = sc.getSeatingData(sc.films[film_selected].showings[giorno_selected].times[orario_selected], cinema_selected)
-    seating.selectBestSeatsByN(3)
+    seating = sc.getSeatingData(orario_data, cinema_selected)
+
+    seats_number = int(input("Quante persone? :"))
+    sc.checkoutSeats(orario_data, seating.selectBestSeatsByN(seats_number), cinema_selected)
+
+    a = sc.SESSION.cookies.get_dict()
+    browser = webdriver.Chrome()
+    browser.get("https://www.thespacecinema.it")
+    for cookie in a.keys():
+        browser.delete_cookie(cookie)
+        browser.add_cookie({"name":cookie,"value":a[cookie], "domain":"thespacecinema.it", "path":"/"})
+    browser.get("https://www.thespacecinema.it/prenotare-il-biglietto/review/"+cinema_selected+"/"+sc.getFilmidBySeating(sc.films[film_selected].showings[giorno_selected].times[orario_selected])+"/"+sc.getFilmSessionidBySeating(sc.films[film_selected].showings[giorno_selected].times[orario_selected]))
+    print("https://www.thespacecinema.it/prenotare-il-biglietto/review/"+cinema_selected+"/"+sc.getFilmidBySeating(sc.films[film_selected].showings[giorno_selected].times[orario_selected])+"/"+sc.getFilmSessionidBySeating(sc.films[film_selected].showings[giorno_selected].times[orario_selected]))
+    while (True):
+        pass
 
 if __name__ == '__main__':
     main()
